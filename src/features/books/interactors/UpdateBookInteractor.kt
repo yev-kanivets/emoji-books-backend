24d30004cd4d5ef3bd10a99de01f04782a1fdc9c
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2021-03-07 17:19:21 +0200

    Add UpdateBookInteractor.

---
+++ src/features/books/interactors/UpdateBookInteractor.kt
@@ -0,0 +1,27 @@
+package com.ykanivets.emojibooks.features.books.interactors
+
+import com.ykanivets.emojibooks.features.books.models.BookBody
+import com.ykanivets.emojibooks.features.books.repository.BooksRepository
+
+class UpdateBookInteractor(
+    private val booksRepository: BooksRepository = BooksRepository.instance
+) {
+
+    data class Request(
+        val bookId: String,
+        val bookBody: BookBody
+    )
+
+    sealed class Response {
+
+        object Success : Response()
+
+        object Failure : Response()
+    }
+
+    suspend fun execute(request: Request): Response = with(request) {
+        val book = bookBody.toBook(id = bookId)
+        booksRepository.update(book)
+        return Response.Success
+    }
+}
