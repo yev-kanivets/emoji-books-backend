c2b472cea6d12027a95589d65fcf121abd51ac74
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2021-03-07 17:19:08 +0200

    Add AddBookInteractor.

---
+++ src/features/books/interactors/AddBookInteractor.kt
@@ -0,0 +1,27 @@
+package com.ykanivets.emojibooks.features.books.interactors
+
+import com.ykanivets.emojibooks.features.books.models.BookBody
+import com.ykanivets.emojibooks.features.books.repository.BooksRepository
+import java.util.*
+
+class AddBookInteractor(
+    private val booksRepository: BooksRepository = BooksRepository.instance
+) {
+
+    data class Request(
+        val bookBody: BookBody
+    )
+
+    sealed class Response {
+
+        object Success : Response()
+
+        object Failure : Response()
+    }
+
+    suspend fun execute(request: Request): Response = with(request) {
+        val book = bookBody.toBook(id = UUID.randomUUID().toString())
+        booksRepository.insert(book)
+        return Response.Success
+    }
+}
